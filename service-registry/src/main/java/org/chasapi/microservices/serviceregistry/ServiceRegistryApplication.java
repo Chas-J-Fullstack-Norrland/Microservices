57f8deea2fa8e4fe748877894a06f3f6be416b9a
Joso1207 <joso1207@student.miun.se>
2026-06-16 12:24:49 +0200

    Package structure + build.gradle dependencies

---
+++ service-registry/src/main/java/org/chasapi/microservices/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,12 @@
+package org.chasapi.microservices.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+}
