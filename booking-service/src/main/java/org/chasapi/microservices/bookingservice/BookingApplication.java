c0bed23541694182b4617dc9002daf4d3279e1d6
NoaEdlund <noaedlund@gmail.com>
2026-06-25 23:10:25 +0200

    Här kommer lite Cache om det behövs

--- booking-service/src/main/java/org/chasapi/microservices/bookingservice/BookingApplication.java
+++ booking-service/src/main/java/org/chasapi/microservices/bookingservice/BookingApplication.java
@@ -1,4 +1,4 @@
-package org.chasapi.microservies.bookingservice;
+package org.chasapi.microservices.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
