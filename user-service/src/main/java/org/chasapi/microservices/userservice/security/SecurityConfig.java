89cd425291a0203a036755d4043e7bc567f92f31
Svartakatten <rikexhx@gmail.com>
2026-06-26 11:54:23 +0200

    added simple jwt functionality

---
+++ user-service/src/main/java/org/chasapi/microservices/userservice/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.chasapi.microservices.userservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oath2 -> oath2
+                        .jwt(jwt -> {})
+                );
+
+        return http.build();
+    }
+}
